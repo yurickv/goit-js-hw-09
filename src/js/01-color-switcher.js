30c8ba9d350bdade243193d69b808d50fcc80ccc
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-13 14:56:57 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const onStartBTN = document.querySelector('[data-start]');
+const StopBTN = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+onStartBTN.addEventListener("click", changeColor);
+StopBTN.addEventListener("click", stopColor);
+
+function changeColor() {
+    timerId = setInterval(interval, 1000);
+}
+
+function interval() {
+    console.log('start button')
+    document.body.style.backgroundColor = getRandomHexColor();
+    onStartBTN.setAttribute('disabled', '');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function stopColor() {
+
+    clearInterval(timerId);
+    onStartBTN.removeAttribute('disabled');
+}
